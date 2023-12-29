98246b3f03c66ab086a5edf2a271b8bc0d1e3b4f
felko <alpha.zeta546@gmail.com>
2023-12-29 16:44:50 +0100

    kakoune: more selections
    kakoune: fix broken reference to fzf

--- flake.nix
+++ flake.nix
@@ -10,9 +10,14 @@
       url = "github:dj95/zjstatus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    kks-source = {
+      url = "github:kkga/kks";
+      flake = false;
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs, flake-utils, zjstatus-source, ... }:
+  outputs = inputs @ { self, nixpkgs, flake-utils, zjstatus-source, kks-source, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -22,6 +27,23 @@
         kakoune = pkgs.kakoune;
         broot = pkgs.broot;
         lazygit = pkgs.lazygit;
+        kks = with pkgs; with lib;
+          buildGoModule rec {
+            pname = "kks";
+            version = "v0.3.8";
+            src = kks-source;
+
+            vendorHash = "sha256-E4D9FGTpS9NkZy6tmnuI/F4dnO9bv8MVaRstxVPvEfo=";
+            subPackages = [ "." ];
+
+            ldflags = [ "-X main.version=${version}" "-X main.buildSource=nix" ];
+
+            postInstall = ''
+              install -Dm555 scripts/kks-* -t $out/bin
+            '';
+          };
+
+				fzf = pkgs.fzf;
         zjstatus = zjstatus-source.packages.${system}.default;
 
         kakoune-startup = pkgs.writeShellScript "vide-kakoune-startup.sh" ''
@@ -39,11 +61,27 @@
           ${kakoune}/bin/kak -n $session_arg -e 'rename-client main' -E 'source ${kakoune-config}'
         '';
         select-file = pkgs.writeShellScript "vide-select-file.sh" ''
-          selected="$(${broot}/bin/broot --conf ${./broot/select.toml})"
+          selected="$(${broot}/bin/broot --conf ${./broot/select-file.toml})"
           if [ -n "$selected" ]; then
-            echo evaluate-commands -client "$2" edit "$selected" | kak -p "$1"
+            echo evaluate-commands -client "$2" edit-or-buffer "$selected" | kak -p "$1"
           else
             echo evaluate-commands -client "$2" echo "no file selected" | kak -p "$1"
+          fi
+        '';
+        select-directory = pkgs.writeShellScript "vide-select-directory.sh" ''
+          selected="$(${broot}/bin/broot --conf ${./broot/select-directory.toml} --only-folders)"
+          if [ -n "$selected" ]; then
+            echo evaluate-commands -client "$2" change-directory "$selected" | kak -p "$1"
+          else
+            echo evaluate-commands -client "$2" echo "no directory selected" | kak -p "$1"
+          fi
+        '';
+        select-buffer = pkgs.writeShellScript "vide-select-buffer.sh" ''
+          selected="$(KKS_SESSION="$1" KKS_CLIENT="$2" ${kks}/bin/kks get %val{buflist} | ${fzf}/bin/fzf)"
+          if [ -n "$selected" ]; then
+            echo evaluate-commands -client "$2" buffer "$selected" | kak -p "$1"
+          else
+            echo evaluate-commands -client "$2" echo "no buffer selected" | kak -p "$1"
           fi
         '';
         kakouneTheme = pkgs.substituteAll {
@@ -54,7 +92,9 @@
           name = "vide.kak";
           src = ./vide.kak;
           selectFile = select-file;
-	  theme = kakouneTheme;
+          selectDirectory = select-directory;
+          selectBuffer = select-buffer;
+	        theme = kakouneTheme;
         };
         session-name-generator = pkgs.writeShellScript "vide-session-name-generator.sh" ''
           echo "$(basename $(pwd))" | tr '[:upper:]' '[:lower:]' | tr -cd 'a-z0-9-'
--- vide.kak
+++ vide.kak
@@ -15,18 +15,63 @@ add-highlighter global/ number-lines -relative
 # editing options
 set-option global tabstop 2
 set-option global scrolloff 5,10
+
+
+# general commands
+
+define-command edit-or-buffer -params 0.. %{
+   evaluate-commands %sh{
+        if [ -n "$1" ]; then
+            if [ -e "$2" ] || [ $2 -eq 0 ]; then
+                for buf in "$kak_buflist"; do
+                    if [ "$buf" == "$1" ]; then
+                        echo buffer "$1"
+                        exit
+                    fi
+                done
+                echo edit "$1"
+            else
+                echo edit "$1" "$2"
+            fi
+        fi
+   }
+}
+
 # user modes
 declare-user-mode file
+declare-user-mode buffer
+map -docstring "file" global user f ': enter-user-mode file<ret>'
+map -docstring "buffer" global user b ': enter-user-mode buffer<ret>'
 
-map global user f ': enter-user-mode file<ret>
 
-# file
-define-command vide-select-file %{
+## file
+
+define-command -docstring 'Select a file to open' file-select %{
     evaluate-commands %sh{
         zellij run --close-on-exit --floating --cwd $(dirname "$kak_buffile") --name select -- @selectFile@ $kak_session $kak_client
     }
 }
 
-map -docstring "Select file" global file f ': vide-select-file<ret>'
+define-command -docstring 'Select a directory in which to cd' file-select-directory %{
+    evaluate-commands %sh{
+        zellij run --close-on-exit --floating --cwd $(dirname "$kak_buffile") --name select -- @selectDirectory@ $kak_session $kak_client
+    }
+}
 
+map -docstring "select" global file f ': file-select<ret>'
+map -docstring "cd" global file d ': file-select-directory<ret>'
+
+
+## buffer
+
+define-command -docstring 'Select a buffer to open' buffer-select %{
+    evaluate-commands %sh{
+        zellij run --close-on-exit --floating --name select -- @selectBuffer@ $kak_session $kak_client
+    }
+}
+
+map -docstring "select" global buffer b ': buffer-select<ret>'
+map -docstring "delete" global buffer q ': delete-buffer<ret>'
+map -docstring "scratch" global buffer s ': edit -scratch<ret>'
+map -docstring "debug" global buffer d ': edit -debug<ret>'
 
